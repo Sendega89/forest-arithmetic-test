6771da46c9f4edf48eca126b9289b5465589e600
Sendega89 <Sendega89@gmail.com>
2025-03-24 14:31:13 +0200

    deploy finish

--- test-register-win/src/api/api.ts
+++ test-register-win/src/api/api.ts
@@ -7,6 +7,7 @@ export const instance = axios.create({
 
 });
 
+
 instance.interceptors.request.use((config) => {
 	const token = localStorage.getItem("tokenTest");
 	config.headers.Authorization = `Bearer ${token}`;
