6b589d220b3736775df16129aff673524acf543f
Sendega89 <Sendega89@gmail.com>
2025-03-24 11:19:41 +0200

    deploy

--- test-register-win/src/api/api.ts
+++ test-register-win/src/api/api.ts
@@ -6,12 +6,12 @@ export const instance = axios.create({
 
 });
 
-instance.interceptors.request.use((config) => {
+/*instance.interceptors.request.use((config) => {
 	const token = localStorage.getItem("tokenTest");
 	config.headers.Authorization = `Bearer ${token}`;
 
 	return config;
-});
+});*/
 
 export const userAPI = {
 	login(email: string, password: string) {
